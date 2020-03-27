e76ca6ef577adcb64e7930268f7ee1b26b5bace9
Saima Akhtar <saimaakhtar@gmail.com>
2020-03-27 21:27:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -70,10 +70,6 @@ def adding_matz
 
 	programmer_hash = 
  		{
-      :yukihiro_matsumoto => {
-        :known_for => "Ruby",
-        :languages => ["LISP", "C"]
-      }
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -87,6 +83,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
+    programmer_hash<< [:yukihiro_matsumoto][:known_for]["Ruby"][:languages]["LISP", "C"]
 end
 
 def changing_alan
