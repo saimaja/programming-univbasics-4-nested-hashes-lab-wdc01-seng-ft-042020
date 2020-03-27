6c593bbafabdfb965bcabc84dbe072609c9fbe04
Saima Akhtar <saimaakhtar@gmail.com>
2020-03-27 20:54:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,8 +15,9 @@ def hopper
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:grace_hopper]
 end
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -38,6 +39,7 @@ def alan_kay_is_known_for
     }
 
 end
+puts programmer_hash[:alan_kay][:known_for]
 
 def dennis_ritchies_language
 	programmer_hash = 
