b6298c86c0c0c23f5882f50f0cd90fa2e1c58f77
Saima Akhtar <saimaakhtar@gmail.com>
2020-03-27 21:24:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,8 +82,11 @@ def adding_matz
         :known_for => "Unix",
         :languages => ["C"]
       }
+      :yukihiro_matsumoto => {
+        :known_for => "Ruby",
+        :languages => ["LISP", "C"]
+      }
     }
-
 end
 
 def changing_alan
