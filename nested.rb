d65c27d62961425503809adb8b88cb7d494f8413
Saima Akhtar <saimaakhtar@gmail.com>
2020-03-27 21:02:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -21,7 +21,6 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	return programmer_hash[:alan_kay][:known_for]
 
 	programmer_hash = 
  		{
@@ -40,7 +39,7 @@ def alan_kay_is_known_for
     }
 
 end
-puts programmer_hash[:alan_kay][:known_for]
+programmer_hash[:alan_kay][:known_for]
 
 def dennis_ritchies_language
 	programmer_hash = 
